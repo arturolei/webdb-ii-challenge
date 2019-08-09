da3684535cf634c4d1cd3bc7175d0b4558db177a
arturolei <artie.lei@gmail.com>
2019-08-08 21:39:26 -0700

    Fleshed out schema in migration file but will probably think things over before making migration.

--- data/migrations/20190808210818_create-cars.js
+++ data/migrations/20190808210818_create-cars.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('cars', (table) => {
+        table.increments('id');
+        table.decimal('VIN').unique().notNullable();
+        table.text('model', 128).notNullable();
+        table.decimal('mileage').notNullable();
+        table.text('transmission',128);
+        table.text('title',128);
+    })
   
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
