7b8c495e9fcb8dea1a344715ccc0808f620bafe6
arturolei <artie.lei@gmail.com>
2019-08-10 09:58:40 -0700

    MVP complete. Stretch Goal 1: use seeds complete.

--- data/migrations/20190808210818_create-cars.js
+++ data/migrations/20190808210818_create-cars.js
@@ -3,7 +3,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('cars', (table) => {
         table.increments('id');
         table.text('VIN', 128).unique().notNullable();
-        table.text('model', 128).notNullable();
+        table.text('make', 128).notNullable();
+        table.integer('model').notNullable();
         table.decimal('mileage').notNullable();
         table.text('transmission',128);
         table.text('title',128);
